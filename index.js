4eadc1dd198f869da96dedeb70b5699e5df75e94
prabhu-raja <prabhuraja@gmail.com>
2020-05-17 08:22:49 -0400

    4 - More than 1 callback

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log('Before');
 getUser(11, usr => {
-  console.log('Usr Detail', usr);
+  console.log('Usr - ', usr);
+  getRepositories(usr.gitHubUserName, repos => {
+    console.log('Repos - ', repos);
+  });
 });
 console.log('After');
 
@@ -8,6 +11,13 @@ function getUser(id, callback) {
   setTimeout(() => {
     console.log('Reading a user from DB...');
     callback({ id: id, gitHubUserName: 'PR'});
-  }, 2000);  
+  }, 1000);  
+}
+
+function getRepositories(name, callback) {
+  setTimeout(() => {
+    console.log(`Getting Reposttories for ${name}`);
+    callback(['repo1', 'repo2', 'repo3'])
+  }, 1000);
 }
 
