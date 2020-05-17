0fb39d52f99c88a67a85e6875c3213490b267c8d
prabhu-raja <prabhuraja@gmail.com>
2020-05-17 08:16:38 -0400

    3 - Simple Callback ☺️

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 console.log('Before');
-const user = getUser(11);
-console.log(user); //undefined
+getUser(11, usr => {
+  console.log('Usr Detail', usr);
+});
 console.log('After');
 
-function getUser(id) {
+function getUser(id, callback) {
   setTimeout(() => {
     console.log('Reading a user from DB...');
-    return { id: id, gitHubUserName: 'PR'};
+    callback({ id: id, gitHubUserName: 'PR'});
   }, 2000);  
 }
 
