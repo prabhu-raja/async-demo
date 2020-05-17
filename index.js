ec86528c62910b0c69974d09e3037141aa696c6a
prabhu-raja <prabhuraja@gmail.com>
2020-05-17 19:51:33 -0400

    6 - Using Async & await

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 console.log('Before');
 
+/*
+  * Promise approach
 getUser(11)
   .then(usr => getRepositories(usr.gitHubUserName))
   .then(repos => console.log(`Repos - ${repos}`))
   .catch(err => console.log('Error', err.message));
+*/
+
+// * Async & Await approach
+async function displayRepos() {
+  try {
+    const usr = await getUser(11);
+    const repos = await getRepositories(usr.gitHubUserName);
+    console.log(`Repos - ${repos}`);
+  } catch (error) {
+    console.log('Error', error.message)
+  }
+}
+displayRepos();
+
 
 console.log('After');
 
@@ -11,6 +27,7 @@ function getUser(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve({ id: id, gitHubUserName: 'PR'});
+      reject(new Error('Some error in getUser 🧐'));
     }, 1000);
   });
 }
@@ -20,6 +37,7 @@ function getRepositories(name) {
     setTimeout(() => {
       console.log(`Getting Repos for ${name}`);
       resolve(['repo1', 'repo2', 'repo3']);
+      reject(new Error('Some error in getRepositories  🧐'));
     }, 1000);
   });
 }
